0ac919fc57b6fbba863260c8db226849224f799a
divatepratik10 <pratikrdivate3210@gmail.com>
2022-04-30 01:24:22 +0530

    [Pratik] added : Create Contact Programme

---
+++ AddressBook/AddBookMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddBookMain
+    {
+        static void Main()
+        {
+            Console.WriteLine("WELCOME TO ADDRESS-BOOK PROGRAMME.");
+            Contacts contacts = new Contacts();
+            contacts.AddContact();
+            Console.ReadLine();      
+        }
+        
+    }
+}
---
+++ AddressBook/Contacts.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Contacts
+    {
+        public string FName;
+        public string LName;
+        public string Address;
+        public string City;
+        public string State;
+        public int ZIP;
+        public long Phone;
+        public string Email;
+        public List<Contacts> ContactList = new List<Contacts>();
+
+        public Contacts()
+        {
+
+        }
+        public Contacts(string FName, string LName, string Address, string City, string State, int ZIP, long Phone, string Email)
+        {
+            this.FName = FName;
+            this.LName = LName;
+            this.Address = Address;
+            this.City = City;
+            this.State = State;
+            this.ZIP = ZIP;
+            this.Phone = Phone;
+            this.Email = Email;
+        }
+
+        public void AddContact()
+        {
+            Console.WriteLine("Enter First Name : ");
+            string Fname = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            string Lname = Console.ReadLine();
+            Console.WriteLine("Enter Address : ");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter City : ");
+            string City = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            string State = Console.ReadLine();
+            Console.WriteLine("Enter ZIP code : ");
+            int ZIP = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Mobile Number : ");
+            long Phone = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Email ID : ");
+            string Email = Console.ReadLine();
+            Contacts OBJcontacts = new Contacts(Fname,Lname,Address,City,State,ZIP,Phone,Email);
+            ContactList.Add(OBJcontacts);
+            Console.WriteLine("Contact is Added.");
+        }
+    }
+
+}
